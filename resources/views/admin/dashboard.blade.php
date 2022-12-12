88a547f206d49e28c4ea9000765846b4d479b917
Jenaro Lopez <jenaro.lopez@hoolisoftware.com>
2022-12-12 23:55:16 +0800

    fix dashboard ui

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,14 +1,44 @@
 @extends('layouts/main')
 @section('content_body')
     <div class="container mp-container">
-        <div class="row no-gutters mp-mt4">
+        <div class="row row no-gutters mp-mt2">
+            <div class="col-12 mp-text-right"  style="display: flex; flex-direction: row; justify-content: right">
+                <div class="row no-gutters">
+                    <div class="mp-ph2 mp-pv2">
+                        <a href="#" id="generate_summary" class="mp-button mp-button--primary mp-button--ghost mp-button--raised mp-button--mini mp-text-fs-small">
+                            Print Report
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row no-gutters ">
             <div class="col mp-ph2 mp-pv2">
                 <div class="mp-card mp-card--plain mp-pv4">
                     <div class="row align-items-center">
                         <div class="col-lg-4">
+                            <div id="campusSelector" class="mp-dropdown mp-ph3">
+                                <a href="#" class="mp-dropdown__toggle mp-link mp-link--accent">
+                                    <span class="mp-text-fs-xxlarge campus_title">
+                                        All UP Campuses
+                                    </span>
+                                    <i class="mp-icon icon-arrow-down mp-ml2"></i>
+                                </a>
+                                <div class="mp-dropdown__menu">
+                                    <a value="All" class="mp-dropdown__item mp-link mp-link--normal campus_change" style="cursor: pointer">All UP Campuses</a>
+                                    @foreach ($campuses as $row)
+                                    <a value="{{ $row->id }}" class="mp-dropdown__item mp-link mp-link--normal campus_change" style="cursor: pointer">
+                                        {{ $row->name }}
+                                    </a>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4" hidden>
                             <select name="" class="mp-text-field mp-ph3 mp-link mp-link--accent"
                                 style="width: 100%; font-size:20px" id="campuses_select">
-                                <option value="">All Campuses</option>
+                                <option value="All">All Campuses</option>
                                 @foreach ($campuses as $row)
                                     <option value="{{ $row->id }}">{{ $row->name }}</option>
                                 @endforeach
@@ -48,9 +78,6 @@
                                         class="mp-button mp-button--primary mp-button--ghost mp-button--raised mp-button--mini mp-text-fs-small">
                                         View All Loans
                                     </a>                                   
-                                    <a href="#" id="generate_summary" class="mp-button mp-button--primary mp-button--ghost mp-button--raised mp-button--mini mp-text-fs-small">
-                                        Print Report
-                                    </a>
                                 </div>
                             </div>
                         </div>
@@ -209,7 +236,9 @@
         });
         var campuses_id;
         $('#campuses_select').on('change', function(e) {
-             campuses_id = $(this).val();
+
+            campuses_id = $(this).val() != 'All' ? $(this).val() : null;
+
             $.ajax({
                 url: "/admin/count_percampuses",
                 method: "GET",
@@ -232,6 +261,9 @@
                     $('#label').text(response.label);
                 },
                 complete: function(response) {
+                    var select = document.querySelector('#campuses_select')
+                    var output = select.options[select.selectedIndex].textContent;
+                    document.querySelector('.campus_title').textContent = output;
                     $('#loading').hide();
                 }
             });
@@ -242,5 +274,13 @@
                 var url = "{{ URL::to('/admin/summaryreports/') }}" + '/' + id; //YOUR CHANGES HERE...
                 window.location.href = url;
             });
+
+        var campus_title = "ALL UP Campuses";
+        $('.campus_change').on('click', function(e) { 
+            var select = document.querySelector('#campuses_select')
+            select.value = e.target.getAttribute('value');
+            select.dispatchEvent(new Event('change'));
+            
+        });
     </script>
 @endsection
